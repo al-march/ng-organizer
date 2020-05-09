33b76404be9914846d09d95fe9b816e784a04be0
Alexandr <valverin95@gmail.com>
2020-05-09 21:10:13 +0300

    feat: dateService: added method to change month

--- src/app/shared/date.service.ts
+++ src/app/shared/date.service.ts
@@ -7,4 +7,9 @@ import { BehaviorSubject } from 'rxjs';
 })
 export class DateService {
   public date: BehaviorSubject<moment.Moment> = new BehaviorSubject(moment());
+
+  changeMonth(dir: number) {
+    const value = this.date.value.add(dir, 'month');
+    this.date.next(value)
+  }
 }
