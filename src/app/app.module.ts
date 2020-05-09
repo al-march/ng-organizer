a7c51e39aa89c18f1f3c56c7884452829ee1a285
Alexandr <valverin95@gmail.com>
2020-05-09 20:55:55 +0300

    feat: added momentPipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { CalendarComponent } from './calendar/calendar.component';
 import { SelectorComponent } from './selector/selector.component';
 import { OrganizerComponent } from './organizer/organizer.component';
+import { MomentPipe } from './shared/moment.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     CalendarComponent,
     SelectorComponent,
-    OrganizerComponent
+    OrganizerComponent,
+    MomentPipe
   ],
   imports: [
     BrowserModule,
